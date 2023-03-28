25525b1c19b2c1c4eabc7aeaa9ebf8a04b33d30d
matheus <matheusvalencasilva2022@gmail.com>
2023-03-28 16:48:12 -0300

    User entity and resource

---
+++ src/main/java/com/matheusdev/demo/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.matheusdev.demo.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.matheusdev.demo.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Matheus", "matheus@gmail.com", "99999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
