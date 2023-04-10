d9026bb6a79c4faee7666fea3a43f06fb3ffcce1
matheus <matheusvalencasilva2022@gmail.com>
2023-04-10 17:26:48 -0300

    Payment, one-to-one association

--- src/main/java/com/matheusdev/demo/config/TestConfig.java
+++ src/main/java/com/matheusdev/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.matheusdev.demo.entities.Category;
 import com.matheusdev.demo.entities.Order;
 import com.matheusdev.demo.entities.OrderItem;
+import com.matheusdev.demo.entities.Payment;
 import com.matheusdev.demo.entities.Product;
 import com.matheusdev.demo.entities.User;
 import com.matheusdev.demo.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 	
