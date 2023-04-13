607b5eb000392ac1238a5470ce0adf4960381371
matheus <matheusvalencasilva2022@gmail.com>
2023-04-13 16:24:03 -0300

    Exception handling - findById

--- src/main/java/com/matheusdev/demo/services/UserService.java
+++ src/main/java/com/matheusdev/demo/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.matheusdev.demo.entities.User;
 import com.matheusdev.demo.repositories.UserRepository;
+import com.matheusdev.demo.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -23,7 +24,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj =  repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
---
+++ src/main/java/com/matheusdev/demo/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.matheusdev.demo.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+
+
+}
